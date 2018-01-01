e6bdcc0814ebe4ba91e92527564ffffbdadd923e
yilmazalican <alican.yilmaz@isik.edu.tr>
2018-01-01 06:06:32 +0300

    selam

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Selam bu benim alan adim bundan once alip 3-4 sene geri vermeyen arkadas, alan adi artik benim ve benim kalacak! :). \nSimdilik Node-js instance'i uzerinde calisiyor. Ileride buralar degerlenecek. :) \nhttp://www.linkedin.com/in/yilmazalican");
+    response.end("Selam ben Alican. \nhttp://www.linkedin.com/in/yilmazalican");
 
 });
 
